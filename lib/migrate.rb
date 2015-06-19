d81359ad83999287f668b11d1052e00c97bafb9e
Mike Horn <mhorn918@gmail.com>
2015-06-19 00:48:23 -0400

    fix primary_key typo

--- lib/migrate.rb
+++ lib/migrate.rb
@@ -1,10 +1,11 @@
 require 'sequel'
 
 ENV["RACK ENV"] ||= "development"
+
 DB = Sequel.connect "sqlite://db/#{ENV["RACK_ENV"]}.sqlite3"
 
 DB.create_table :images do
-  primary key :id
+  primary_key :id
   String :title
   String :file
 end
