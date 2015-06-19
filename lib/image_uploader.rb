796f7058305bade7669f48f1821190e24333a9a6
Mike Horn <mhorn918@gmail.com>
2015-06-19 01:33:30 -0400

    update resize_to_fit

--- lib/image_uploader.rb
+++ lib/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
   version :small do
-    process :resize_to_fit, [720, 720]
+    process resize_to_fit: [720, 720]
   end
 
 end
